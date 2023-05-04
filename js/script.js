69a5b262f1633678eca2aac69e56567bd819d284
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-05-04 12:28:51 +0200

    Commit 7

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ menuBtn.addEventListener('click', function (e) {
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark');
 });
+
+// Typing Effect
+
+let typed = new Typed('.auto-input',{
+    strings: [
+        'Front-End Developer!',
+        'Freelancer!',
+        'UI Designer!',
+        'Youtuber!',
+    ],
+    typedpeed: 100,
+    backspeed: 100,
+    backDelay: 2000,
+    loop: true
+
+});
